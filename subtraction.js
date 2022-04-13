f3e66b2dc2e79e83195f38fdd98ca5de224f81ec
rohan-ap <rohan.ap21@gmail.com>
2022-04-13 17:40:40 +0530

    modified prog of sub

--- subtraction.js
+++ subtraction.js
@@ -1,6 +1,7 @@
 function subtract(){
-var a
-var b
+var a = document.getElementById("first no").value
+var b = document.getElementById("second no").value
 var result 
 result = a-b
+document.getElementById("result").value=result
 }
