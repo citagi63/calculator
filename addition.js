8f1f5b68ff6948cd5ffdf53618c49be254e2aba5
Rakshitha <rakshithahr2022@gmail.com>
2022-04-13 17:41:22 +0530

    modified add

--- addition.js
+++ addition.js
@@ -1,8 +1,9 @@
 
 function add() {
-	var a
-	var b
+	var a = document.getElementById("first no").value
+	var b = document.getElementById("second no").value
 	var result
 	result=a+b
+	document.getElementById("result").value=result
 }
 
